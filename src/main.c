f3f85c9087f485087842887dea77a7614b53a638
çağla arslan <caglaarslan@gmail.com>
2026-07-05 00:51:14 +0300

    added showMenu() and createFile() functions

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void showMenu();
 void createFile();
 void readFile();
 void writerFile();
@@ -10,34 +11,11 @@ void searchFile();
 
 
 int main(){
-    int n;
-    printf("============================\n");
-    printf("     SMART FİLE MANAGER     \n");
-    printf("============================\n");
-    printf("1. Create File\n");
-    printf("2. Read File\n");
-    printf("3. Write To File\n");
-    printf("4. Rename File\n");
-    printf("5. Delete File\n");
-    printf("6. Search File\n");
-    printf("7. Exit\n");
-
-    printf("Choose an option:");
-    scanf("%d",&n);
-    
-   
-
-    if(n<=0 || n>=8)
-    {
-       printf("Wrong choice! Please select a number between 1 and 7. ");
-    } 
-    else
-    {
-       printf("you selected: %d\n",n);
-    }
+   int n;
 
    while(1)
    {
+      showMenu();
       scanf("%d",&n);
 
       switch (n)
@@ -70,8 +48,46 @@ int main(){
             return 0;
     
          default:
-             printf("Wrong choice ! Please select a number between 1 and 7.\n");
+            printf("Wrong choice ! Please select a number between 1 and 7.\n");
       }  
    }
     return 0;
+}
+
+void showMenu()
+{
+   printf("============================\n");
+   printf("     SMART FİLE MANAGER     \n");
+   printf("============================\n");
+   printf("1. Create File\n");
+   printf("2. Read File\n");
+   printf("3. Write To File\n");
+   printf("4. Rename File\n");
+   printf("5. Delete File\n");
+   printf("6. Search File\n");
+   printf("7. Exit\n");
+
+   printf("Choose an option:");
+}
+
+void createFile()
+{
+   char fileName[100];
+   FILE* fptr;
+   printf("Create File seleced.\n");
+   printf("Enter file name: ");
+   scanf("%99s", fileName);
+   fptr= fopen(fileName,"w");
+
+   if(fptr==NULL)
+   {
+      printf("File  could not be created.\n");
+   }
+
+   else
+   {
+      printf("File created successfully.\n");
+      fclose(fptr);
+   }
+
 }
